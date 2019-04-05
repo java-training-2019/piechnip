05601106176e26fd1384b4c9ac235b832478b0e5
qtr-ptc-70 <qtr-ptc-70@ptc.local>
2019-04-05 16:28:10 +0200

    third message

--- src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
+++ src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
@@ -4,7 +4,9 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.hasXPath;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
 
 class TradeEventControllerTest {
 
@@ -12,18 +14,33 @@ class TradeEventControllerTest {
     void tradeEvent() {
         RestAssured
                 .given()
-                .when().get("/tradeEvent")
+                .when().get("/tradeEvent/123")
                 .then().statusCode(200);
     }
+
+    //test that currency should have 3 uppercase characters
+    //controller sjould return random currency from a list
+
     @Test
     void shouldGet404OnNonExistingPage() {
         when().get("/nonExisting").then().statusCode(404);
     }
 
     @Test
-    void shouldReturnTradesWithVersion0() {
-        when().get("/tradeEvent").then()
+    void shouldReturnTradesWithIdAndVersion() {
+        String id= "sampleId";
+        when().get("/tradeEvent/{id}", id).then()
                 .statusCode(200)
-                .body("tradeEvent.version", equalTo("0"));
+                .body("tradeEvent.version", equalTo("0"))
+                .body("tradeEvent.id", equalTo(id))
+                .body(not(hasXPath("tradeEvent/tradeLocation")));
+    }
+
+    @Test
+    void shouldHaveTradeLocationIfFromObs() {
+        when().get("/tradeEvent/OBS-123")
+                .then()
+                .body(hasXPath("/tradeEvent/tradeLocation"));
+
     }
 }
